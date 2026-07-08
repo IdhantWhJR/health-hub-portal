6966da9996b0d049de398d09e61c3b0891b15298
Idhant Sood <108124363+IdhantWhJR@users.noreply.github.com>
2026-07-08 19:19:44 +0530

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,7 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
-import router from "./routes";
+import { pinoHttp } from "pino-http"; // was: import pinoHttp from "pino-http"import router from "./routes";
 import { logger } from "./lib/logger";
 
 const app: Express = express();
